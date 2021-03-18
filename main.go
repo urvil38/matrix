c31c447296c7711853eeb731329b754557d79561
Urvil Patel <patelurvil38@gmail.com>
2021-03-18 18:54:24 +0530

    Add Dockerfile

--- main.go
+++ main.go
@@ -9,28 +9,28 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func getEnv(env string) string {
-	e := os.Getenv(env)
-	return e
+func getEnv(env string, def string) string {
+	val, ok := os.LookupEnv(env)
+	if !ok {
+		return def
+	}
+	return val
 }
 
 func main() {
-	port := getEnv("PORT")
-	if port == "" {
-		port = "8000"
-	}
+	port := getEnv("PORT", "5000")
 
 	log.Println("server is running on port:", port)
 	r := mux.NewRouter()
 
 	staticHandler := http.FileServer(http.Dir("static"))
-	r.PathPrefix("/static/").Handler(loggingHandler(http.StripPrefix("/static/", staticHandler)))
+	r.PathPrefix("/static/").Handler(loggingHandler(noCache(http.StripPrefix("/static/", staticHandler))))
 
 	r.Handle("/", loggingHandler(staticHandler))
 
 	srv := &http.Server{
 		Handler: r,
-		Addr:    ":"+port,
+		Addr:    ":" + port,
 		// Good practice: enforce timeouts for servers you create!
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
@@ -39,4 +39,11 @@ func main() {
 	if err := srv.ListenAndServe(); err != nil {
 		log.Println(err)
 	}
+}
+
+func noCache(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "no-cache, private, max-age=0")
+		h.ServeHTTP(w, r)
+	})
 }
--- static/js/matrix.js
+++ static/js/matrix.js
@@ -1,56 +1,74 @@
-var streams = [];
-var symbolSize = 15;
+let streams = [];
+let symbolSize = 15;
+let r, g, b, l
 
-function setup(){
-	var x = 0;
-	var y = random(-1000,0);
-	createCanvas(window.innerWidth,window.innerHeight);
-	background(255,255,255);
+function luminance(r, g, b) {
+	return 0.2126 * r + 0.7152 * g + 0.0722 * b
+}
+
+function setup() {
+	let x = 0;
+	let y = random(-1000, 0);
+	r = random(0,255)
+	g = random(0,255)
+	b = random(0,255)
+	l = Math.round(luminance(r, g, b))
+	if (l < 128) {
+		background(255, 255, 255);
+	} else {
+		background(0);
+	}
+
+	createCanvas(window.innerWidth, window.innerHeight);
 	textSize(symbolSize);
-	for(var i=0;i<= width/symbolSize;i++){
-		var stream = new Stream();
+	for (let i = 0; i <= width / symbolSize; i++) {
+		let stream = new Stream();
 		streams.push(stream);
-		stream.generateSymbol(x,y);
-		x += symbolSize;		
+		stream.generateSymbol(x, y);
+		x += symbolSize;
 	}
 }
 
-function draw(){
-	background(0);
-	streams.forEach(function(stream){
-		stream.render();
+function draw() {
+	if (l < 128) {
+		background(255, 255, 255);
+	} else {
+		background(0);
+	}
+	streams.forEach(function (stream) {
+		stream.render(r, g, b);
 	});
 }
 
-function Symbol(x,y,speed,first){
+function Symbol(x, y, speed, first) {
 	this.x = x;
 	this.y = y;
 	this.value;
 	this.speed = speed;
-	this.setInterval = round(random(2,100));
+	this.setInterval = round(random(2, 100));
 	this.first = first;
 
-	this.setToRandomSymbol = function(){
-		if(frameCount % this.setInterval == 0){
-			this.value = String.fromCharCode(0x30A0 + random(0,96));
+	this.setToRandomSymbol = function () {
+		if (frameCount % this.setInterval == 0) {
+			this.value = String.fromCharCode(0x30A0 + random(0, 96));
 		}
 	}
 
-	this.rain = function(){
+	this.rain = function () {
 		this.y = (this.y >= height) ? 0 : this.y += this.speed;
 	}
 }
 
-function Stream(){
+function Stream() {
 	this.symbols = [];
-	this.totalSymbols = round(random(5,20));
-	this.speed = random(2,14);
+	this.totalSymbols = round(random(5, 20));
+	this.speed = random(2, 14);
 
-	this.generateSymbol = function(x,y){
+	this.generateSymbol = function (x, y) {
 
-		var first = round(random(0,4)) == 1;
-		for(var i=0;i<=this.totalSymbols;i++){
-			symbol = new Symbol(x,y,this.speed,first);
+		let first = round(random(0, 4)) == 1;
+		for (let i = 0; i <= this.totalSymbols; i++) {
+			symbol = new Symbol(x, y, this.speed, first);
 			symbol.setToRandomSymbol();
 			this.symbols.push(symbol);
 			y -= symbolSize;
@@ -58,14 +76,14 @@ function Stream(){
 		}
 	}
 
-	this.render = function(){
-		this.symbols.forEach(function(symbol){
-			if(symbol.first){
-				fill(255,0,0);
-			}else{
-				fill(255,0,10);
+	this.render = function (r, g, b) {
+		this.symbols.forEach(function (symbol) {
+			if (symbol.first) {
+				fill(255, 0, 10);
+			} else {
+				fill(r, g, b);
 			}
-			text(symbol.value,symbol.x,symbol.y);
+			text(symbol.value, symbol.x, symbol.y);
 			symbol.rain();
 			symbol.setToRandomSymbol();
 		});
